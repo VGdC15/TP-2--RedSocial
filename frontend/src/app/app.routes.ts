d6b4da95df70379962a0e4f3689f70d17d66b7ae
VGdC15 <veronicac153@gmail.com>
2025-06-04 15:05:49 -0300

    creacion de carpetas principales y rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PublicacionesComponent } from './pages/publicaciones/publicaciones.component';
+import { MiPerfilComponent } from './pages/mi-perfil/mi-perfil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'registro', component: RegistroComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'publicaciones', component: PublicacionesComponent },
+    { path: 'perfil', component: MiPerfilComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '**', redirectTo: 'login' }
+];
