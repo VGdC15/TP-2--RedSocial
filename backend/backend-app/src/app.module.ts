57acb3d2585b774ed88ca1f8195c40d861240712
VGdC15 <veronicac153@gmail.com>
2025-06-05 18:13:15 -0300

    - instalación de mongoose - configuracion de eslint

--- backend/backend-app/src/app.module.ts
+++ backend/backend-app/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PublicacionesModule } from './publicaciones/publicaciones.module';
 import { AutenticacionModule } from './autenticacion/autenticacion.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PublicacionesModule, AutenticacionModule, UsuariosModule],
+  imports: [PublicacionesModule, AutenticacionModule, UsuariosModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
