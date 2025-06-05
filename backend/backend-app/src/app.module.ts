b379e716380caa1a9a5bb38c6e20922a39ff00bd
VGdC15 <veronicac153@gmail.com>
2025-06-05 19:02:41 -0300

    conexion a mongoose

--- backend/backend-app/src/app.module.ts
+++ backend/backend-app/src/app.module.ts
@@ -5,10 +5,13 @@ import { PublicacionesModule } from './publicaciones/publicaciones.module';
 import { AutenticacionModule } from './autenticacion/autenticacion.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PublicacionesModule, AutenticacionModule, UsuariosModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [PublicacionesModule, AutenticacionModule, UsuariosModule, ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI!)],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
